5316edf7342b743bc5edc3adcbe5e99cafd778a1
jemblonganvalley <jemblonganvalley@gmail.com>
2023-04-01 02:42:08 +0700

    uupate

--- server.js
+++ server.js
@@ -16,69 +16,50 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")))
 
 
+
+const initializeVenom = async (req, res, next) => {
+    req.client = await venom.create();
+    next();
+};
+
+app.use(initializeVenom);
 app.use("/api", auth_route)
 
-venom.create()
-    .then(client => {
-        console.info(client)
-        start(client)
+
+app.get("/api/read", (req, res) => {
+    return res.status(200).json({
+        success: true
     })
-    .catch(err => {
-        console.error(err)
-    })
+})
 
-const start = (client) => {
+// api kirim pesan
+app.post("/api/send_message", auth_check, async (req, res) => {
+    try {
+        const { to, text, client } = await req.body
+        const sendMessage = await client.sendText(`${to}@c.us`, text)
 
-    // ketika ada pesan masuk
-    // client.onMessage((msg) => {
-    //     // random jawaban
-    //     let rj = Math.floor(Math.random() * jawaban.length) // 0 - 4
-
-    //     if (pesanMasuk.includes(msg.body) && msg.isGroupMsg === false) {
-    //         client.sendText(msg.from, jawaban[rj])
-    //             .then(result => {
-    //                 console.info(result)
-    //             })
-    //             .catch(err => {
-    //                 console.error(err)
-    //             })
-    //     }
-    // })
-
-    app.get("/api/read", (req, res) => {
-        return res.status(200).json({
-            success: true
-        })
-    })
-
-    // api kirim pesan
-    app.post("/api/send_message", auth_check, async (req, res) => {
-        try {
-            const { to, text } = await req.body
-            const sendMessage = await client.sendText(`${to}@c.us`, text)
-
-            if (sendMessage.erro) {
-                res.status(401).json({
-                    success: false,
-                    error: sendMessage.erro
-                })
-                return
-            }
-
-            return res.status(200).json({
-                success: true,
-                msg: "Pesan berhasil terkirim"
-            })
-
-        } catch (error) {
-            res.status(500).json({
+        if (sendMessage.erro) {
+            res.status(401).json({
                 success: false,
-                error: error.message
+                error: sendMessage.erro
             })
+            return
         }
-    })
 
-}
+        return res.status(200).json({
+            success: true,
+            msg: "Pesan berhasil terkirim"
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        })
+    }
+})
+
+
 
 app.listen(PORT, "0.0.0.0", () => {
     console.info("server berjalan")
